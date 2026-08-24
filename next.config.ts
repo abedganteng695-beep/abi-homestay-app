4b0c14d90ca2b7b4817d80aa9455d7b64bc1d25f
vereniaes <titasari_1519622036@mhs.unj.ac.id>
2026-08-24 14:37:50 +0700

    Update Prisma datasource to use MONGODB_URI

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/index.html",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/kamar.html",
+        destination: "/kamar",
+        permanent: true,
+      },
+      {
+        source: "/penghuni.html",
+        destination: "/penghuni",
+        permanent: true,
+      },
+      {
+        source: "/laporan.html",
+        destination: "/laporan",
+        permanent: true,
+      },
+      {
+        source: "/pengaturan.html",
+        destination: "/pengaturan",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
